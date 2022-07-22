47efbafcce5730da82763c36d37d2c0c0c081a69
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-22 14:45:08 -0400

    Add repositories gallery to repositories view

--- src/components/Repositories/Repositories.jsx
+++ src/components/Repositories/Repositories.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { GithubContext } from '../../providers/github-provider'
+import RepositoriesGallery from "./RepositoriesGallery"
+import * as S from './styled'
 
 export default function Repositories() {
+  const { githubState } = useContext(GithubContext);
+  const [repositoryList, setRepositoryList] = useState(githubState.repositories);
+  
+  const showRepositories = () => setRepositoryList(githubState.repositories);
+
+  const showStarred = () => setRepositoryList(githubState.starred);
+
+  useEffect(()=>{
+    setRepositoryList(githubState.repositories);
+  }, [githubState])
+
   return (
-    <div>Repositories</div>
+    <div>
+      <S.ButtonsContainer>
+        <S.Button onClick={showRepositories}>Own Repositories</S.Button>
+        <S.Button onClick={showStarred}>Starred Repositories</S.Button>
+      </S.ButtonsContainer>
+      <RepositoriesGallery repositoriesList={repositoryList}>
+
+      </RepositoriesGallery>
+    </div>
   )
 }
